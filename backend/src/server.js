6eb2c1a102ba8f64bbbca5b8c174714b17647607
subodh kumar matho <subodh@samarth.ac.in>
2025-12-21 16:39:37 +0530

    cors error fix it

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,8 @@ const PORT = process.env.PORT || 3000;
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://student-teacher-management-system-iahm1654l.vercel.app"
+    "https://student-teacher-management-system-iahm1654l.vercel.app",
+    "https://student-teacher-managem-git-679121-subodh-kumar-mahtos-projects.vercel.app" //  Add this
   ],
   credentials: true // important for cookies
 }));
